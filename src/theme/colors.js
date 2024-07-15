4b749dafc68ad9e8546e7975c11bb35a007c573f
mochsenhofer <123185718+mochsenhofer@users.noreply.github.com>
2024-07-15 23:57:53 +0200

    Create colors.js

---
+++ src/theme/colors.js
@@ -0,0 +1,5 @@
+export const darkBlue = "#1D3461"; // primary
+export const white = "#FFFFFF"; // secondary
+export const medBuddyBlue = "#0f6bff"; // tertiary
+export const backgroundWhite = "#FFFFFF"; // background
+export const black = "#000000"; // text
