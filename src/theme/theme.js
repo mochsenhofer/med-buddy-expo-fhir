9a09ead0a78851b94eea7ee29e5c7fd4d893ef50
mochsenhofer <123185718+mochsenhofer@users.noreply.github.com>
2024-07-15 23:58:00 +0200

    Create theme.js

---
+++ src/theme/theme.js
@@ -0,0 +1,22 @@
+import { MD3LightTheme as DefaultTheme } from "react-native-paper";
+import {
+  darkBlue,
+  white,
+  medBuddyBlue,
+  backgroundWhite,
+  black,
+} from "./colors";
+
+const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: darkBlue,
+    secondary: white,
+    tertiary: medBuddyBlue,
+    background: backgroundWhite,
+    text: black,
+  },
+};
+
+export default theme;
