1f0c931e8240ad715a7945710fd6675ce47e2a21
mochsenhofer <123185718+mochsenhofer@users.noreply.github.com>
2024-07-16 23:46:44 +0200

    Update renderUserInput.js

--- src/utils/renderFunctions/renderUserInput.js
+++ src/utils/renderFunctions/renderUserInput.js
@@ -1,14 +1,31 @@
-import React from "react";
-import { Text, TextInput } from "react-native-paper";
+import React, { useState } from "react";
+import { Text, TextInput, useTheme } from "react-native-paper";
 import { userInputStyles } from "../../theme/styles";
 import { View } from "react-native";
+import { DatePickerInput } from "react-native-paper-dates";
 
 export default function renderUserInput({ item }) {
+  const [inputDate, setInputDate] = useState(null);
+  const theme = useTheme();
   switch (item.type) {
     case "display":
       return <View style={userInputStyles.displayText} />;
     case "integer":
-      return <Text variant="titleSmall">Integer</Text>;
+      return (
+        <TextInput
+          label={item.placeholder}
+          placeholder={item.question}
+          mode="outlined"
+          style={userInputStyles.textInput}
+          onChangeText={item.onChangeText}
+          ref={item.ref}
+          autoFocus={item.autoFocus}
+          textColor="black"
+          maxLength={item.maxLength}
+          keyboardType={item.keyboardType}
+          onSubmitEditing={item.onSubmit}
+        />
+      );
     case "choice":
       return <Text variant="titleSmall">Choice</Text>;
     case "string":
@@ -23,6 +40,21 @@ export default function renderUserInput({ item }) {
           autoFocus={item.autoFocus}
           textColor="black"
           maxLength={item.maxLength}
+          keyboardType={item.keyboardType}
+          onSubmitEditing={item.onSubmit}
+          iconColor={theme.colors.primary}
+        />
+      );
+    case "date":
+      return (
+        <DatePickerInput
+          locale={item.locale}
+          style={userInputStyles.textInput}
+          label={item.placeholder}
+          value={inputDate}
+          onChange={setInputDate}
+          mode="outlined"
+          validRange={{ startDate: new Date(1900, 0, 1), endDate: new Date() }}
         />
       );
     default:
