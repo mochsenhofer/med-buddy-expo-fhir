d7c3eb7c0ba32812793e93a9346233fe05e625a8
mochsenhofer <123185718+mochsenhofer@users.noreply.github.com>
2024-07-16 20:21:32 +0200

    Update RegistrationScreen.js

--- src/screens/RegistrationScreen.js
+++ src/screens/RegistrationScreen.js
@@ -1,20 +1,72 @@
 import React from "react";
-import renderSectionListItem from "../utils/renderFunctions/renderSectionListItem";
+import renderQuestionItem from "../utils/renderFunctions/renderQuestionItem";
 import BasicScreenLayout from "../components/common/BasicScreenLayout";
 import { previewScreenRoute } from "../navigation/MedBuddyStackNavigation";
+import {
+  updateGivenName,
+  updateFamilyName,
+  updateDateOfBirth,
+  updateInsuranceNumber,
+  updateGender,
+} from "../store/patientReducer";
+import { useDispatch } from "react-redux";
 
 export default function RegistrationScreen() {
-  const testData = [
+  const dispatch = useDispatch();
+  const patientInputArray = [
     {
-      title: "Main dishes",
-      data: ["Pizza", "Burger", "Risotto"],
+      title: "Registration Screen",
+      data: [
+        {
+          question: "Please answer the following questions: ",
+          type: "display",
+        },
+        {
+          question: "First Name",
+          placeholder: "What is your first name?",
+          type: "string",
+          onChangeText: (text) => {
+            dispatch(updateGivenName(text));
+          },
+          autofocus: true,
+        },
+        {
+          question: "Last Name",
+          placeholder: "What is your last name?",
+          type: "string",
+          onChangeText: (text) => {
+            dispatch(updateFamilyName(text));
+          },
+        },
+        {
+          question: "Date of birth",
+          placeholder: "What is your date of birth?",
+          type: "string",
+          onChangeText: (text) => {
+            dispatch(updateDateOfBirth(text));
+          },
+        },
+        {
+          question: "Insurance number",
+          placeholder: "What is your insurance number?",
+          type: "string",
+          onChangeText: (text) => {
+            dispatch(updateInsuranceNumber(text));
+          },
+          maxLength: 10,
+          keyboardType: "numeric",
+        },
+        {
+          question: "What is your gender?",
+          type: "choice",
+        },
+      ],
     },
   ];
   return (
     <BasicScreenLayout
-      title={"RegistrationScreen"}
-      sections={testData}
-      renderItem={renderSectionListItem}
+      sections={patientInputArray}
+      renderItem={renderQuestionItem}
       navigateTo={previewScreenRoute}
     />
   );
