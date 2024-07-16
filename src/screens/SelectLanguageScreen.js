237427f7e11c2200c72e2379befafab405f97c78
mochsenhofer <123185718+mochsenhofer@users.noreply.github.com>
2024-07-16 12:36:58 +0200

    Update SelectLanguageScreen.js

--- src/screens/SelectLanguageScreen.js
+++ src/screens/SelectLanguageScreen.js
@@ -1,19 +1,56 @@
 import React from "react";
 import CenteredScreenLayout from "../components/common/CenteredScreenLayout";
-import renderSectionListItem from "../utils/renderFunctions/renderSectionListItem";
+import renderSelectLanguageButton from "../utils/renderFunctions/renderSelectLanguageButton";
+import { Button } from "react-native-paper";
+import { useNavigation } from "@react-navigation/native";
+import { registrationScreenRoute } from "../navigation/MedBuddyStackNavigation";
 
 export default function SelectLanguageScreen() {
-  const selectLanguageSections = [
+  const navigation = useNavigation();
+  const languageButtonProps = [
     {
-      title: "Select Language",
-      data: ["English", "Spanish", "French"],
+      title: null,
+      data: [
+        {
+          label: "Willkommen!",
+          description: "Für Deutsch hier klicken",
+          key: "de",
+          image: require("../assets/images/austria.png"),
+        },
+      ],
+    },
+    {
+      title: null,
+      data: [
+        {
+          label: "Welcome!",
+          description: "For English click",
+          key: "en",
+          image: require("../assets/images/english.png"),
+        },
+      ],
+    },
+    {
+      title: null,
+      data: [
+        {
+          label: "Witamy",
+          description: "Kliknij, aby po polsku",
+          key: "pl",
+          image: require("../assets/images/polish.png"),
+        },
+      ],
     },
   ];
+
   return (
-    <CenteredScreenLayout
-      title={"Select Language"}
-      sections={selectLanguageSections}
-      renderItem={renderSectionListItem}
-    />
+    <>
+      <CenteredScreenLayout
+        title={"Select Language"}
+        sections={languageButtonProps}
+        renderItem={renderSelectLanguageButton}
+        numColumns={3}
+      />
+    </>
   );
 }
