bac0961e9ec8655ab7e87c731e179c8e7ff649f8
mochsenhofer <123185718+mochsenhofer@users.noreply.github.com>
2024-07-16 20:21:23 +0200

    Update Section.js

--- src/components/common/Section.js
+++ src/components/common/Section.js
@@ -1,32 +1,23 @@
 import React from "react";
-import { KeyboardAvoidingView, SectionList, StyleSheet } from "react-native";
+import { KeyboardAvoidingView, SectionList } from "react-native";
+import { sectionListStyles } from "../../theme/styles";
 import renderSectionHeader from "../../utils/renderFunctions/renderSectionHeader";
 
 export default function Section({ sections, renderItem }) {
   return (
-    <KeyboardAvoidingView style={styles.sectionContainer} behavior="padding">
+    <KeyboardAvoidingView
+      style={sectionListStyles.sectionContainer}
+      behavior="padding"
+    >
       <SectionList
         sections={sections}
         keyExtractor={(item, index) => item + index}
         renderItem={renderItem}
         renderSectionHeader={renderSectionHeader}
         stickySectionHeadersEnabled={false}
-        scrollEnabled={false}
+        scrollEnabled={true}
+        style={sectionListStyles.sectionList}
       />
     </KeyboardAvoidingView>
   );
 }
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    flex: 4,
-    justifyContent: "center",
-    alignItems: "flex-start",
-    width: "75%",
-    backgroundColor: "yellow",
-  },
-  headerText: {
-    color: "white",
-    textAlign: "center",
-  },
-});
