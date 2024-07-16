4a83a4c26f086cbe95d3c12468d9081f4ae2409d
mochsenhofer <123185718+mochsenhofer@users.noreply.github.com>
2024-07-16 20:22:18 +0200

    Update renderSectionListItem.js

--- src/utils/renderFunctions/renderSectionListItem.js
+++ src/utils/renderFunctions/renderSectionListItem.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Text } from "react-native-paper";
+import { paragraphText } from "../../theme/constants";
 
 export default function renderSectionListItem({ item }) {
-  return <Text variant="titleLarge">{item}</Text>;
+  return <Text variant={paragraphText}>{item}</Text>;
 }
