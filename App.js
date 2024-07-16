75005cb974a2671216dc61b0de419cd83587d6a4
mochsenhofer <123185718+mochsenhofer@users.noreply.github.com>
2024-07-16 20:20:55 +0200

    add store

--- App.js
+++ App.js
@@ -1,13 +1,16 @@
 import React from "react";
+import { Provider as ReduxProvider } from "react-redux";
+import { store } from "./src/store/store";
 import { PaperProvider } from "react-native-paper";
 import MedBuddyStackNavigation from "./src/navigation/MedBuddyStackNavigation";
 import theme from "./src/theme/theme";
 
 export default function App() {
   return (
-    // add store here
-    <PaperProvider theme={theme}>
-      <MedBuddyStackNavigation />
-    </PaperProvider>
+    <ReduxProvider store={store}>
+      <PaperProvider theme={theme}>
+        <MedBuddyStackNavigation />
+      </PaperProvider>
+    </ReduxProvider>
   );
 }
