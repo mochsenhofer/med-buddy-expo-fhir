8a9335f3d97b75f22ec5ad96e66ccd8136a37007
mochsenhofer <123185718+mochsenhofer@users.noreply.github.com>
2024-07-16 20:22:15 +0200

    Update renderSectionHeader.js

--- src/utils/renderFunctions/renderSectionHeader.js
+++ src/utils/renderFunctions/renderSectionHeader.js
@@ -1,12 +1,17 @@
 import React from "react";
 import { Text } from "react-native-paper";
 import theme from "../../theme/theme";
+import { sectionHeader } from "../../theme/constants";
 
 export default function renderSectionHeader({ section: { title } }) {
   return (
     <Text
-      variant="headlineSmall"
-      style={{ color: theme.colors.primary, textTransform: "uppercase" }}
+      variant={sectionHeader}
+      style={{
+        color: theme.colors.primary,
+        textTransform: "uppercase",
+        marginBottom: 10,
+      }}
     >
       {title}
     </Text>
