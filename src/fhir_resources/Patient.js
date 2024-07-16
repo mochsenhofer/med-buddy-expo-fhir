b534a3753936d63784255534aec811c1cebcca31
mochsenhofer <123185718+mochsenhofer@users.noreply.github.com>
2024-07-16 20:21:28 +0200

    Create Patient.js

---
+++ src/fhir_resources/Patient.js
@@ -0,0 +1,39 @@
+export const Patient = {
+  resourceType: "Patient",
+  id: "",
+  gender: "",
+  birthDate: "",
+  identifier: [
+    {
+      use: "usual",
+      type: {
+        coding: [
+          {
+            system: "http://terminology.hl7.org/CodeSystem/v2-0203",
+            code: "SS",
+          },
+        ],
+      },
+      system: "urn:oid:1.2.36.146.595.217.0.1",
+      value: "",
+    },
+  ],
+  name: [
+    {
+      family: "",
+      given: [""],
+    },
+  ],
+  communication: [
+    {
+      language: {
+        coding: [
+          {
+            system: "urn:ietf:bcp:47",
+            code: "", // en - english, de- german, pl - polish
+          },
+        ],
+      },
+    },
+  ],
+};
