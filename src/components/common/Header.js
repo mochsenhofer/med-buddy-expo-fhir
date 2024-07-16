271551e8adba7631896be925d478fa41a4eca7ab
mochsenhofer <123185718+mochsenhofer@users.noreply.github.com>
2024-07-16 12:54:12 +0200

    Update Header.js

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,17 +1,23 @@
 import React from "react";
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, View, Image } from "react-native";
 import { Text } from "react-native-paper";
 import { useTheme } from "react-native-paper";
+import { edgeComponentMargins } from "../../theme/constants";
+
+function MedBuddyCornerLogo() {
+  return (
+    <Image
+      source={require("../../assets/images/logo.png")}
+      style={styles.cornerLogo}
+    />
+  );
+}
 
 export default function Header({ title }) {
   const theme = useTheme();
   return (
-    <View
-      style={[
-        { backgroundColor: theme.colors.primary },
-        styles.headerContainer,
-      ]}
-    >
+    <View style={[styles.headerContainer]}>
+      <MedBuddyCornerLogo />
       <Text variant="headlineSmall" style={styles.headerText}>
         {title}
       </Text>
@@ -21,9 +27,16 @@ export default function Header({ title }) {
 
 const styles = StyleSheet.create({
   headerContainer: {
-    flex: 1.25,
+    flex: 1,
     justifyContent: "center",
     alignItems: "center",
     width: "100%",
+  },
+  cornerLogo: {
+    position: "absolute",
+    top: edgeComponentMargins,
+    left: edgeComponentMargins,
+    width: 80,
+    height: 80,
   },
 });
