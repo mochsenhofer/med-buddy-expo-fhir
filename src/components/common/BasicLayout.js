1d9912157889bc8a8153e0ac738adb1c53af9ef1
mochsenhofer <123185718+mochsenhofer@users.noreply.github.com>
2024-07-19 22:57:52 +0200

    Update BasicLayout.js

--- src/components/common/BasicLayout.js
+++ src/components/common/BasicLayout.js
@@ -1,14 +1,33 @@
 import React from "react";
 import Header from "./basic_layout_components/Header";
 import SectionListComponent from "./basic_layout_components/SectionListComponent";
-import Footer from "./basic_layout_components/Footer";
+import FooterNavigation from "./basic_layout_components/FooterNavigation";
+import { View, StyleSheet } from "react-native";
 
-export default function BasicLayout({ sections, renderItem, title }) {
+export default function BasicLayout({
+  title,
+  sections,
+  renderItem,
+  handleNextButtonPress,
+  navigateTo,
+}) {
   return (
-    <>
+    <View style={styles.container}>
       <Header title={title} />
       <SectionListComponent sections={sections} renderItem={renderItem} />
-      <Footer />
-    </>
+      <FooterNavigation
+        handleNextButtonPress={handleNextButtonPress}
+        navigateTo={navigateTo}
+      />
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    width: "100%",
+  },
+});
