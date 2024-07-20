f1435e6742382203530033077836e90a15c9a8fa
mochsenhofer <123185718+mochsenhofer@users.noreply.github.com>
2024-07-20 10:06:21 +0200

    Update LanguageSelectionScreen.js

--- src/screens/LanguageSelectionScreen.js
+++ src/screens/LanguageSelectionScreen.js
@@ -1,33 +1,42 @@
 import React from "react";
-import CenteredLayout from "../components/common/CenteredLayout";
-import { registrationScreenRoute } from "../navigation/Navigation";
+import FlatListLayout from "../components/common/FlatListLayout";
 import renderSelectLanguageButton from "../utils/renderSelectLanguageButton";
+import { useNavigation } from "@react-navigation/native";
+import { registrationScreenRoute } from "../navigation/Navigation";
 
 export default function LanguageSelectionScreen() {
-  const DATA = [
+  const navigation = useNavigation();
+  function selectLanguage(language) {
+    console.log(language);
+    navigation.navigate(registrationScreenRoute);
+  }
+  const languageSelectionScreenData = [
     {
-      title: null,
-      data: [
-        {
-          label: "English",
-          value: "en",
-        },
-        {
-          label: "Spanish",
-          value: "es",
-        },
-        {
-          label: "French",
-          value: "fr",
-        },
-      ],
+      label: "Welcome!",
+      description: "For English click here",
+      value: "en",
+      onPress: (value) => selectLanguage(value),
+      icon: require("../assets/images/english.png"),
+    },
+    {
+      label: "Willkommen!",
+      description: "Für Deutsch hier klicken",
+      value: "de",
+      onPress: (value) => selectLanguage(value),
+      icon: require("../assets/images/austria.png"),
+    },
+    {
+      label: "Witamy!",
+      description: "Kliknij na polski",
+      value: "pl",
+      onPress: (value) => selectLanguage(value),
+      icon: require("../assets/images/polish.png"),
     },
   ];
 
   return (
-    <CenteredLayout
-      title={"Select Language"}
-      sections={DATA}
+    <FlatListLayout
+      data={languageSelectionScreenData}
       renderItem={renderSelectLanguageButton}
       navigateTo={registrationScreenRoute}
       numColumns={3}
