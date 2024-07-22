96dea68f880c357e0441300f31af5a58dcd8332b
mochsenhofer <123185718+mochsenhofer@users.noreply.github.com>
2024-07-22 20:14:26 +0200

    Update RadioButtonsComponent.js

--- src/components/functional/user_input_components/RadioButtonsComponent.js
+++ src/components/functional/user_input_components/RadioButtonsComponent.js
@@ -1,21 +1,55 @@
 import React, { useState } from "react";
 import { Button } from "react-native-paper";
+import { FlatList } from "react-native";
+import { useTheme } from "react-native-paper";
 
 function RadioButton({ text, selected, onSelect }) {
+  const theme = useTheme();
   return (
-    <Button onPress={onSelect} mode={selected ? "contained-tonal" : "outlined"}>
+    <Button
+      style={styles.radioButtonStyle}
+      onPress={onSelect}
+      mode={selected ? "contained" : "outlined"}
+      buttonColor={selected ? theme.colors.tertiary : null}
+      labelStyle={{ fontSize: 20 }}
+    >
       {text}
     </Button>
   );
 }
 
-export default function RadioButtons() {
+export default function RadioButtons({ options, onSelect }) {
   const [selected, setSelected] = useState(false);
+
+  function handleSelect(value) {
+    setSelected(value);
+    onSelect(value);
+  }
   return (
-    <RadioButton
-      text={"Hallo"}
-      selected={selected}
-      onSelect={() => setSelected(true)}
+    <FlatList
+      style={styles.radioButtonFlatList}
+      data={options}
+      numColumns={3}
+      keyExtractor={(item) => item.valueCoding.code}
+      renderItem={({ item }) => (
+        <RadioButton
+          text={item.valueCoding.text}
+          selected={selected === item.valueCoding.code}
+          onSelect={() => handleSelect(item.valueCoding.code)}
+        />
+      )}
     />
   );
 }
+
+const styles = {
+  radioButtonFlatList: {
+    width: "100%",
+    alignItems: "space-between",
+  },
+  radioButtonStyle: {
+    width: "32%",
+    marginHorizontal: 5,
+    borderWidth: 2,
+  },
+};
