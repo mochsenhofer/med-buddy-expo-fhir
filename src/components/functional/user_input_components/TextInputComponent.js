aeb8e77f666e943e05a5322fbce562713abf6d95
mochsenhofer <123185718+mochsenhofer@users.noreply.github.com>
2024-07-22 11:36:20 +0200

    Create TextInputComponent.js

---
+++ src/components/functional/user_input_components/TextInputComponent.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { StyleSheet } from "react-native";
+import { TextInput } from "react-native-paper";
+
+export default function TextInputComponent({ item }) {
+  return (
+    <TextInput
+      mode="outlined"
+      label={item.text}
+      onChangeText={item.onChangeText}
+      inputMode={item.type === "integer" ? "numeric" : "text"}
+      maxLength={item.maxLength}
+      value={item.value}
+      onSubmitEditing={item.onSubmitEditing}
+      ref={item.ref}
+      autoFocus={item.autoFocus}
+      style={userInputStyles.textInput}
+    />
+  );
+}
+
+export const userInputStyles = StyleSheet.create({
+  textInput: {
+    marginVertical: 5,
+    height: 50,
+    justifyContent: "center",
+  },
+});
