473b43d791e64a61ec5bed96cc5800495048135f
mochsenhofer <123185718+mochsenhofer@users.noreply.github.com>
2024-07-22 11:36:17 +0200

    Update RegistrationScreen.js

--- src/screens/RegistrationScreen.js
+++ src/screens/RegistrationScreen.js
@@ -1,33 +1,65 @@
-import React from "react";
+import React, { useRef } from "react";
 import BasicLayout from "../components/common/BasicLayout";
 import { previewScreenRoute } from "../navigation/Navigation";
-import renderItem from "../utils/renderItem";
+import renderQuestionItem from "../utils/renderQuestionItem";
 
 export default function RegistrationScreen() {
-  const DATA = [
+  const lastNameRef = useRef(null);
+  const insuranceNumberRef = useRef(null);
+  const dobRef = useRef(null);
+
+  const registrationScreenData = [
     {
-      title: "Main dishes",
-      data: ["Pizza", "Burger", "Risotto"],
-    },
-    {
-      title: "Sides",
-      data: ["French Fries", "Onion Rings", "Fried Shrimps"],
-    },
-    {
-      title: "Drinks",
-      data: ["Water", "Coke", "Beer"],
-    },
-    {
-      title: "Desserts",
-      data: ["Cheese Cake", "Ice Cream"],
+      title: "Please provide your personal information: ",
+      data: [
+        {
+          linkId: "p.1",
+          text: "First Name",
+          value: "",
+          type: "string",
+          onChangeText: (text) => console.log(text),
+          onSubmitEditing: () => lastNameRef.current.focus(),
+          autoFocus: true,
+        },
+        {
+          linkId: "p.2",
+          text: "Last Name",
+          value: "",
+          type: "string",
+          ref: lastNameRef,
+          onSubmitEditing: () => insuranceNumberRef.current.focus(),
+          onChangeText: (text) => console.log(text),
+        },
+        {
+          linkId: "p.3",
+          text: "Insurance Number",
+          value: "",
+          type: "integer",
+          ref: insuranceNumberRef,
+          maxLength: 10,
+          onChangeText: (text) => console.log(text),
+        },
+        {
+          linkId: "p.4",
+          text: "Date of Birth",
+          value: "",
+          type: "date",
+        },
+        {
+          linkId: "p.5",
+          text: "Gender",
+          value: "",
+          type: "choice",
+        },
+      ],
     },
   ];
 
   return (
     <BasicLayout
       title={"Registration"}
-      sections={DATA}
-      renderItem={renderItem}
+      sections={registrationScreenData}
+      renderItem={renderQuestionItem}
       navigateTo={previewScreenRoute}
     />
   );
