9fad0c04b8592f71cda05716929d167bc8a4fd8c
mochsenhofer <123185718+mochsenhofer@users.noreply.github.com>
2024-07-22 13:39:17 +0200

    Update LanguageSelectionScreen.js

--- src/screens/LanguageSelectionScreen.js
+++ src/screens/LanguageSelectionScreen.js
@@ -3,11 +3,28 @@ import FlatListLayout from "../components/common/FlatListLayout";
 import renderSelectLanguageButton from "../utils/renderSelectLanguageButton";
 import { useNavigation } from "@react-navigation/native";
 import { registrationScreenRoute } from "../navigation/Navigation";
+import { en, de, pl, registerTranslation } from "react-native-paper-dates";
+import { useDispatch } from "react-redux";
+import { updateLanguage } from "../store/patientReducer";
 
 export default function LanguageSelectionScreen() {
   const navigation = useNavigation();
+  const dispatch = useDispatch();
+
   function selectLanguage(language) {
-    console.log(language);
+    switch (language) {
+      case "en":
+        registerTranslation("en", en);
+        break;
+      case "de":
+        registerTranslation("de", de);
+
+        break;
+      case "pl":
+        registerTranslation("pl", pl);
+        break;
+    }
+    dispatch(updateLanguage(language));
     navigation.navigate(registrationScreenRoute);
   }
   const languageSelectionScreenData = [
