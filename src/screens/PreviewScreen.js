8d13858d16b192d78057baaa092ddebe05fd7afd
mochsenhofer <123185718+mochsenhofer@users.noreply.github.com>
2024-07-22 22:44:30 +0200

    Update PreviewScreen.js

--- src/screens/PreviewScreen.js
+++ src/screens/PreviewScreen.js
@@ -1,35 +1,34 @@
 import React from "react";
-import { Text } from "react-native-paper";
-import BasicLayout from "../components/common/BasicLayout";
-import { questionnaireScreenRoute } from "../navigation/Navigation";
+import BasicLayoutNoFooterNavigation from "../components/common/BasicLayoutNoFooterNavigation";
+import renderPreviewScreenItem from "../utils/renderPreviewScreenItem";
+import { useNavigation } from "@react-navigation/native";
+import {
+  videoScreenRoute,
+  questionnaireScreenRoute,
+} from "../navigation/Navigation";
 
 export default function PreviewScreen() {
-  const DATA = [
+  const navigation = useNavigation();
+  const previewScreenData = [
     {
-      title: "Main dishes",
-      data: ["Pizza", "Burger", "Risotto"],
-    },
-    {
-      title: "Sides",
-      data: ["French Fries", "Onion Rings", "Fried Shrimps"],
-    },
-    {
-      title: "Drinks",
-      data: ["Water", "Coke", "Beer"],
-    },
-    {
-      title: "Desserts",
-      data: ["Cheese Cake", "Ice Cream"],
+      title: "Preview",
+      data: [
+        {
+          text: "Information",
+          videoTitle: "Video",
+          readInfoTitle: "Information",
+          onVideoClick: () => navigation.navigate(videoScreenRoute),
+          onReadInfoClick: () => navigation.navigate(questionnaireScreenRoute),
+        },
+      ],
     },
   ];
 
-  const renderItem = ({ item }) => <Text>{item}</Text>;
-
   return (
-    <BasicLayout
+    <BasicLayoutNoFooterNavigation
       title={"Preview Screen"}
-      sections={DATA}
-      renderItem={renderItem}
+      sections={previewScreenData}
+      renderItem={renderPreviewScreenItem}
       navigateTo={questionnaireScreenRoute}
     />
   );
