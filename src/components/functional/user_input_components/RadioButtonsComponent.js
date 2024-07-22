356dd4a133038455146a0a1f070f9e347f8290bb
mochsenhofer <123185718+mochsenhofer@users.noreply.github.com>
2024-07-22 14:09:07 +0200

    Create RadioButtonsComponent.js

---
+++ src/components/functional/user_input_components/RadioButtonsComponent.js
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+import { Button } from "react-native-paper";
+
+function RadioButton({ text, selected, onSelect }) {
+  return (
+    <Button onPress={onSelect} mode={selected ? "contained" : "outlined"}>
+      {text}
+    </Button>
+  );
+}
+
+export default function RadioButtons() {
+  const [selected, setSelected] = useState(false);
+  return (
+    <RadioButton
+      text={"Hallo"}
+      selected={selected}
+      onSelect={() => setSelected(true)}
+    />
+  );
+}
