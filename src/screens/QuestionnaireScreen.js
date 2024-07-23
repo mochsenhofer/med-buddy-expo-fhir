f68994d4f97afeedd2f0dfea3b73d760e7cd3a35
mochsenhofer <123185718+mochsenhofer@users.noreply.github.com>
2024-07-23 08:57:27 +0200

    Update QuestionnaireScreen.js

--- src/screens/QuestionnaireScreen.js
+++ src/screens/QuestionnaireScreen.js
@@ -1,34 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
 import BasicLayout from "../components/common/BasicLayout";
+import renderQuestionItem from "../utils/renderQuestionItem";
+import useQuestionnaireData from "../hooks/useQuestionnaireData";
+import { useNavigation } from "@react-navigation/native";
 import { overviewScreenRoute } from "../navigation/Navigation";
-import renderItem from "../utils/renderItem";
 
 export default function QuestionnaireScreen() {
-  const DATA = [
-    {
-      title: "Main dishes",
-      data: ["Pizza", "Burger", "Risotto"],
-    },
-    {
-      title: "Sides",
-      data: ["French Fries", "Onion Rings", "Fried Shrimps"],
-    },
-    {
-      title: "Drinks",
-      data: ["Water", "Coke", "Beer"],
-    },
-    {
-      title: "Desserts",
-      data: ["Cheese Cake", "Ice Cream"],
-    },
+  const navigation = useNavigation();
+  const { informationSections, questionnaireSections, consentSections } =
+    useQuestionnaireData();
+  const [page, setPage] = useState(0);
+  const [sections, setSections] = useState(informationSections);
+
+  const allSections = [
+    informationSections,
+    questionnaireSections,
+    consentSections,
   ];
+  const [sectionIndex, setSectionIndex] = useState(0);
+
+  const currentSection = [sections[page]];
+
+  function onNextButtonPress() {
+    if (page < sections.length - 1) {
+      setPage(page + 1);
+    } else if (sectionIndex < allSections.length - 1) {
+      setSectionIndex(sectionIndex + 1);
+      setSections(allSections[sectionIndex + 1]);
+      setPage(0);
+    } else {
+      navigation.navigate(overviewScreenRoute);
+    }
+  }
+
+  function onBackButtonPress() {
+    if (page > 0) {
+      setPage(page - 1);
+    } else if (sectionIndex > 0) {
+      setSectionIndex(sectionIndex - 1);
+      setSections(allSections[sectionIndex - 1]);
+      setPage(allSections[sectionIndex - 1].length - 1);
+    } else {
+      navigation.goBack();
+    }
+  }
 
   return (
     <BasicLayout
       title={"QuestionnaireScreen"}
-      sections={DATA}
-      renderItem={renderItem}
-      navigateTo={overviewScreenRoute}
+      sections={currentSection}
+      renderItem={renderQuestionItem}
+      onNextButtonPress={onNextButtonPress}
+      onBackButtonPress={onBackButtonPress}
     />
   );
 }
