555ec6fe7124bef8cae2ff443c5416b09217e79b
mochsenhofer <123185718+mochsenhofer@users.noreply.github.com>
2024-07-23 09:01:40 +0200

    Create BasicLayoutProgressbar.js

---
+++ src/components/common/BasicLayoutProgressbar.js
@@ -0,0 +1,35 @@
+import React from "react";
+import Header from "./basic_layout_components/Header";
+import SectionListComponent from "./basic_layout_components/SectionListComponent";
+import FooterNavigation from "./basic_layout_components/FooterNavigation";
+import { View, StyleSheet } from "react-native";
+
+export default function BasicLayoutProgressBar({
+  title,
+  sections,
+  renderItem,
+  onNextButtonPress,
+  onBackButtonPress,
+  navigateTo,
+}) {
+  return (
+    <View style={styles.container}>
+      <Header title={title} />
+      <SectionListComponent sections={sections} renderItem={renderItem} />
+      <FooterNavigation
+        onNextButtonPress={onNextButtonPress}
+        onBackButtonPress={onBackButtonPress}
+        navigateTo={navigateTo}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    width: "100%",
+  },
+});
