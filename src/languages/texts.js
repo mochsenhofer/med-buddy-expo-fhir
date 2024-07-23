2a2c2c6746629f8e8e61dbc133b2d550c658d990
mochsenhofer <123185718+mochsenhofer@users.noreply.github.com>
2024-07-23 12:39:55 +0200

    Create texts.js

---
+++ src/languages/texts.js
@@ -0,0 +1,107 @@
+export const texts = {
+  en: {
+    registrationScreen: {
+      title: "Registration",
+      description: "Please fill out the following fields to register",
+      "p.1": "First Name",
+      "p.2": "Last Name",
+      "p.3": "Insurance Number",
+      "p.4": "Date of Birth",
+      "p.5": "Gender",
+      male: "Male",
+      female: "Female",
+      other: "Other",
+      error: {
+        "p.1": "First name is required",
+        "p.2": "Last name is required",
+        "p.3": "Insurance number is required",
+        "p.4": "Date of birth is required",
+        "p.5": "Gender is required",
+      },
+    },
+    previewScreen: {
+      title: "Overview",
+      description: "Welcome to the study",
+      "p.1": "Please review the information below",
+    },
+    videoScreen: {},
+    questionnaireScreen: {
+      progressBar: {},
+      information: {
+        "i.1": "Dear Patient!",
+        "i.1.1":
+          "You were referred for computer tomography (CT) by your attending doctor. For your information, please read this text and answer the questions on page 2. Afterwards, you will be informed about all important facts and have the opportunity to ask questions.",
+        "i.2": "What is computer tomography?",
+        "i.2.1":
+          "Computer tomography (CT) is a special X-ray examination used to make cross-sectional images of the body. This way, important information on the location of the disease centres can be obtained. This is of crucial importance for further treatment.",
+        "i.3": "How is the examination carried out?",
+        "i.3.1":
+          "The examination takes about 10-15 minutes. During the examination, you lie on a special examination table that moves through the opening on the CT device. Throughout the examination you are monitored by qualified personnel and connected with them via an intercom system.",
+        "i.4": "What do I have to do?",
+        "i.4.1":
+          "In order to achieve optimal image quality, we ask you to remain still during the entire examination. In some cases, we may ask you to follow instructions (e.g.: inhale, exhale, hold breath, do not swallow, etc.).",
+        "i.5": "Risk of X-rays:",
+        "i.5.1":
+          "In very rare cases, X-rays in high doses can contribute to tumour development. In general, the risk of CT X-rays is very low and depends on the type and number of CT and X-ray examinations performed. You have been referred to this CT scan by your attending doctor because this examination is important for diagnosis and the information cannot be obtained by another examination.",
+        "i.6": "Why contrast agent?",
+        "i.6.1":
+          "Contrast agents can increase the information significance of many CT scans and help your doctors to better recognize pathological changes. Contrast agents are administered as infusions into an arm vein during the examination.",
+        "i.7": "Can there be complications?",
+        "i.7.1":
+          "As with any injection, pain, bruising and, very rarely, infection can occur at the injection site. During the application, many people experience warmth and a bitter taste in their mouths. Scientific studies show that more than 99% of all patients tolerate the contrast agents used well. Seldom (about 1 in 400) there is a slight drop in blood pressure and thus slight discomfort such as nausea or pain. Very rarely (about 1 in 10,000) there are severe side effects such as shortness of breath, drop in blood pressure, cardiac arrhythmia or cramps. As with almost any medical procedure, life-threatening complications are possible, but extremely unlikely. If you experience any side effects, immediate medical assistance is always available.",
+        "i.7.2":
+          "In rare cases, contrast agent may leak into the arm at the injection site. This leads to painful swelling that can sometimes last for days and must be treated. If you feel pain or swelling of your arm during the examination, please notify the medical technician who carries out the examination immediately - even during the examination.",
+      },
+      questionnaire: {
+        "q.1":
+          "Please answer the following questions to the best of your knowledge.",
+        "q.1.1": "Size (cm)",
+        "q.1.2": "Weight (kg)",
+        "q.1.3": "Is there a possibility of pregnancy?",
+        "q.2": "Did you have one of these examinations before?",
+        "q.2.1": "Computer tomography (CT)",
+        "q.2.2": "Vascular X-ray (Angiography / Cardiac Catheter)",
+        "q.2.3": "Kidney X-ray (Urography)",
+        "q.2.4": "Leg vein imaging (phlebography)",
+        "q.3":
+          "Did you experience any side-effects after the administration of contrast agents? If so, which ones?",
+        "q.3.1": "Nausea/Vomiting/Suffocation",
+        "q.3.2": "Asthma attack / shortness of breath",
+        "q.3.3": "Rash",
+        "q.3.4": "Seizures / unconsciousness",
+        "q.3.5": "Shivering attacks",
+        "q.4": "Do you suffer from any of the following medical conditions?",
+        "q.4.1": "Allergies that require treatment?",
+        "q.4.2": "of the heart?",
+        "q.4.3": "of the kidney / adrenal gland?",
+        "q.4.4": "of the thyroid gland?",
+        "q.4.5": "Diabetes?",
+        "q.4.6": "Myasthenia gravis? (specific muscle disease)",
+        "q.5":
+          "Please answer the following questions to the best of your knowledge.",
+        "q.5.1": "If yes, which diabetes medications do you take?",
+        "q.5.2": "Do you carry a pacemaker?",
+        "q.yes": "Yes",
+        "q.no": "No",
+        "q.unknown": "asked but unknown",
+        "q.idk": "I don't know",
+      },
+      overview: {},
+      consent: {
+        "c.1": "Consent",
+        "c.1.1": "The patient agrees to the examination: ",
+        "c.2.1": "Signature",
+        "c.2.1":
+          "By signing, I confirm that I have read and understood the text in this form. I have answered the questions to the best of my knowledge and belief. In a personal talk, the risks were explained to me and my additional questions were answered satisfactorily. I agree that the suggested examination should be carried out",
+        clear: "Clear",
+        signature:
+          "Signature of the patient and/or his/her legal representative",
+      },
+    },
+    navigationButtons: {},
+  },
+  de: {
+    registrationScreen: {},
+  },
+  pl: { registrationScreen: {} },
+};
