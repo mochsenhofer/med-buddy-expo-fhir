23c312df9e3d9469d4e9e0ccd9489276ec1fa71e
mochsenhofer <123185718+mochsenhofer@users.noreply.github.com>
2024-07-24 09:31:58 +0200

    Update QuestionnaireScreen.js

--- src/screens/QuestionnaireScreen.js
+++ src/screens/QuestionnaireScreen.js
@@ -1,85 +1,36 @@
-import React, { useState } from "react";
-import BasicLayoutProgressBar from "../components/common/BasicLayoutProgressbar";
-import renderQuestionnaireItem from "../utils/renderQuestionnaireItem";
-import useQuestionnaireData from "../hooks/useQuestionnaireData";
-import { useNavigation } from "@react-navigation/native";
-import { overviewScreenRoute } from "../navigation/Navigation";
-import { useSelector } from "react-redux";
+import React from "react";
 import { Text } from "react-native-paper";
+import BasicLayout from "../components/common/BasicLayout";
+import { overviewScreenRoute } from "../navigation/Navigation";
 
 export default function QuestionnaireScreen() {
-  const navigation = useNavigation();
-  const questionnaireResponseState = useSelector(
-    (state) => state.questionnaireResponse
-  );
-  const {
-    informationSections,
-    sectionWithLinkIdQuestionnaire,
-    questionnaireSections,
-    consentSections,
-  } = useQuestionnaireData();
-  const [page, setPage] = useState(0);
-  const [sectionIndex, setSectionIndex] = useState(0);
-
-  const allSections = [
-    informationSections,
-    questionnaireSections,
-    [{ title: "Overview", data: [{}] }],
-    consentSections,
+  const DATA = [
+    {
+      title: "Main dishes",
+      data: ["Pizza", "Burger", "Risotto"],
+    },
+    {
+      title: "Sides",
+      data: ["French Fries", "Onion Rings", "Fried Shrimps"],
+    },
+    {
+      title: "Drinks",
+      data: ["Water", "Coke", "Beer"],
+    },
+    {
+      title: "Desserts",
+      data: ["Cheese Cake", "Ice Cream"],
+    },
   ];
 
-  const sectionTitles = ["Information", "Questionnaire", "Overview", "Consent"];
-
-  const sections = allSections[sectionIndex];
-  const currentSection = sections[page];
-
-  function findQuestionnaireSectionTitle(linkId) {
-    return sectionWithLinkIdQuestionnaire.item.find(
-      (qItem) => qItem.linkId === linkId
-    ).text;
-  }
-
-  function renderOverviewItem(item) {
-    return <Text>Hallo</Text>;
-  }
-
-  function onNextButtonPress() {
-    if (page < sections.length - 1) {
-      setPage(page + 1);
-    } else if (sectionIndex < allSections.length - 1) {
-      setSectionIndex(sectionIndex + 1);
-      setPage(0);
-    } else {
-      navigation.navigate(overviewScreenRoute);
-    }
-  }
-
-  function onBackButtonPress() {
-    if (page > 0) {
-      setPage(page - 1);
-    } else if (sectionIndex > 0) {
-      setSectionIndex(sectionIndex - 1);
-      setPage(allSections[sectionIndex - 1].length - 1);
-    } else {
-      navigation.goBack();
-    }
-  }
-
-  function handleSelect(value, item) {
-    console.log("clicked" + value);
-  }
+  const renderItem = ({ item }) => <Text>{item}</Text>;
 
   return (
-    <BasicLayoutProgressBar
-      title={sectionTitles[sectionIndex]}
-      sections={[currentSection]}
-      renderItem={({ item }) =>
-        renderQuestionnaireItem({ item, onSelect: handleSelect })
-      }
-      onNextButtonPress={onNextButtonPress}
-      onBackButtonPress={onBackButtonPress}
-      currentStep={sectionIndex}
-      page={`${page + 1}/${sections.length}`}
+    <BasicLayout
+      title={"QuestionnaireScreen"}
+      sections={DATA}
+      renderItem={renderItem}
+      navigateTo={overviewScreenRoute}
     />
   );
 }
