7cb0cc327da113e50634984525441d9c2339fede
mochsenhofer <123185718+mochsenhofer@users.noreply.github.com>
2024-07-24 12:54:57 +0200

    Update questionnaireResponseReducer.js

--- src/store/questionnaireResponseReducer.js
+++ src/store/questionnaireResponseReducer.js
@@ -1,7 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { QuestionnaireResponse } from "../fhir_resources/QuestionnaireResponse";
 
-const initialState = QuestionnaireResponse || {};
+const initialState = QuestionnaireResponse;
 
 export const questionnaireResponseReducer = createSlice({
   name: "questionnaireResponse",
@@ -22,16 +22,73 @@ export const questionnaireResponseReducer = createSlice({
     updateQuestionnaire: (state, action) => {
       state.contained[1] = action.payload;
     },
+    updateValueInteger: (state, action) => {
+      const { linkId, value } = action.payload;
+      // Find the item with the matching linkId
+      const findAndUpdateItem = (items) => {
+        for (let item of items) {
+          if (item.linkId === linkId && item.answer && item.answer[0]) {
+            item.answer[0].valueInteger = value;
+            return true;
+          }
+          if (item.item && findAndUpdateItem(item.item)) {
+            return true;
+          }
+        }
+        return false;
+      };
+
+      findAndUpdateItem(state.item);
+    },
+    updateValueString: (state, action) => {
+      const { linkId, value } = action.payload;
+      // Find the item with the matching linkId
+      const findAndUpdateItem = (items) => {
+        for (let item of items) {
+          if (item.linkId === linkId && item.answer && item.answer[0]) {
+            item.answer[0].valueString = value;
+            return true;
+          }
+          if (item.item && findAndUpdateItem(item.item)) {
+            return true;
+          }
+        }
+        return false;
+      };
+
+      findAndUpdateItem(state.item);
+    },
+    updateValueCoding: (state, action) => {
+      const { linkId, value } = action.payload;
+      // Find the item with the matching linkId
+      const findAndUpdateItem = (items) => {
+        for (let item of items) {
+          if (item.linkId === linkId && item.answer && item.answer[0]) {
+            item.answer[0].valueCoding.code = value;
+            return true;
+          }
+          if (item.item && findAndUpdateItem(item.item)) {
+            return true;
+          }
+        }
+        return false;
+      };
+
+      findAndUpdateItem(state.item);
+    },
   },
 });
 
 // Action creators are generated for each case reducer function
 export const {
-  updateAuthor,
-  updateQuestionnaireId,
-  updateQuestionnaireResponseStatus,
+  updateValueInteger,
+  updateValueString,
+  updateValueCoding,
   updatePatient,
   updateQuestionnaire,
+  updateQuestionnaireResponseStatus,
+  updateQuestionnaireId,
+  updateAuthor,
 } = questionnaireResponseReducer.actions;
 
 export default questionnaireResponseReducer.reducer;
