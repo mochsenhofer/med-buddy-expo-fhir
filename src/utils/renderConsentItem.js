a8d73d90527f556567745f8453b7900ef42c302c
mochsenhofer <123185718+mochsenhofer@users.noreply.github.com>
2024-07-26 22:50:42 +0200

    Update renderConsentItem.js

--- src/utils/renderConsentItem.js
+++ src/utils/renderConsentItem.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useRef } from "react";
 import { Text } from "react-native-paper";
 import { p } from "../theme/constants";
+import { Canvas } from "@benjeau/react-native-draw";
 import RadioButtons from "../components/functional/user_input_components/RadioButtonsComponent";
 import { StyleSheet } from "react-native";
 
@@ -17,7 +18,13 @@ export default function renderConsentItem({ item }) {
           currentValue={item.value}
         />
       ) : (
-        <Text>Signature Pad</Text>
+        <Canvas
+          ref={item.ref}
+          style={styles.signaturePad}
+          width={500}
+          height={250}
+          roundPoints={true}
+        />
       )}
     </>
   );
@@ -27,5 +34,15 @@ const styles = StyleSheet.create({
   text: {
     margin: 5,
     fontSize: 20,
+  },
+  signaturePad: {
+    backgroundColor: "white",
+    flex: 1,
+    width: 800,
+    height: 250,
+    borderRadius: 25,
+    borderWidth: 5,
+    marginBottom: 5,
+    marginTop: 5,
   },
 });
