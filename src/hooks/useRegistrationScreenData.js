1ed05f0e43b464ca84a8658f1f96dcf6769a5e8f
mochsenhofer <123185718+mochsenhofer@users.noreply.github.com>
2024-07-27 08:35:54 +0200

    Update useRegistrationScreenData.js

--- src/hooks/useRegistrationScreenData.js
+++ src/hooks/useRegistrationScreenData.js
@@ -1,4 +1,4 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import {
   updateGivenName,
@@ -7,16 +7,64 @@ import {
   updateBirthDate,
   updateGender,
 } from "../store/patientReducer";
+import useLanguage from "./useLanguage";
 import { texts } from "../languages/texts";
 
 const useRegistrationScreenData = () => {
   const registeredPatient = useSelector((state) => state.patient);
-  const language = registeredPatient.communication[0].language.coding[0].code;
+  const language = useLanguage();
   const dispatch = useDispatch();
   const lastNameRef = useRef(null);
   const insuranceNumberRef = useRef(null);
   const dobRef = useRef(null);
   const text = texts.en.registrationScreen;
+
+  const [formErrors, setFormErrors] = useState({
+    givenNameError: false,
+    familyNameError: false,
+    insuranceNumberError: false,
+    birthDateError: false,
+    genderError: false,
+  });
+
+  function validateField(field) {
+    const errors = { ...formErrors };
+    switch (field) {
+      case "givenName":
+        errors.givenNameError = registeredPatient.name[0].given[0] === "";
+        break;
+      case "familyName":
+        errors.familyNameError = registeredPatient.name[0].family === "";
+        break;
+      case "insuranceNumber":
+        errors.insuranceNumberError =
+          registeredPatient.identifier[0].value.length !== 10;
+
+        break;
+      case "birthDate":
+        errors.birthDateError = registeredPatient.birthDate === "";
+        break;
+      case "gender":
+        errors.genderError = registeredPatient.gender === "";
+        break;
+      default:
+        break;
+    }
+    setFormErrors(errors);
+    return !Object.values(errors).includes(true); // Return false if any errors are true
+  }
+
+  function validateAllFields() {
+    const fields = [
+      "givenName",
+      "familyName",
+      "insuranceNumber",
+      "birthDate",
+      "gender",
+    ];
+    const results = fields.map((field) => validateField(field));
+    return results.every((result) => result);
+  }
 
   const registrationScreenData = [
     {
@@ -27,9 +75,15 @@ const useRegistrationScreenData = () => {
           text: text["p.1"],
           value: registeredPatient.name[0].given[0],
           type: "string",
-          onChangeText: (text) => dispatch(updateGivenName(text)),
+          onChangeText: (text) => {
+            dispatch(updateGivenName(text));
+            setFormErrors({ ...formErrors, givenNameError: false });
+          },
           onSubmitEditing: () => lastNameRef.current.focus(),
+          onEndEditing: () => validateField("givenName"),
           autoFocus: true,
+          error: formErrors.givenNameError,
+          errorMessage: text.error["p.1"],
         },
         {
           linkId: "p.2",
@@ -38,7 +92,13 @@ const useRegistrationScreenData = () => {
           type: "string",
           ref: lastNameRef,
           onSubmitEditing: () => insuranceNumberRef.current.focus(),
-          onChangeText: (text) => dispatch(updateFamilyName(text)),
+          onChangeText: (text) => {
+            dispatch(updateFamilyName(text));
+            setFormErrors({ ...formErrors, familyNameError: false });
+          },
+          onEndEditing: () => validateField("familyName"),
+          error: formErrors.familyNameError,
+          errorMessage: text.error["p.2"],
         },
         {
           linkId: "p.3",
@@ -47,7 +107,13 @@ const useRegistrationScreenData = () => {
           type: "integer",
           ref: insuranceNumberRef,
           maxLength: 10,
-          onChangeText: (text) => dispatch(updateInsuranceNumber(text)),
+          onChangeText: (text) => {
+            dispatch(updateInsuranceNumber(text));
+            setFormErrors({ ...formErrors, insuranceNumberError: false });
+          },
+          onEndEditing: () => validateField("insuranceNumber"),
+          error: formErrors.insuranceNumberError,
+          errorMessage: text.error["p.3"],
         },
         {
           linkId: "p.4",
@@ -55,26 +121,37 @@ const useRegistrationScreenData = () => {
           value: registeredPatient.birthDate,
           type: "date",
           ref: dobRef,
-          onChangeText: (text) => dispatch(updateBirthDate(text)),
+          onChangeText: (text) => {
+            dispatch(updateBirthDate(text));
+            setFormErrors({ ...formErrors, birthDateError: false });
+          },
+          onEndEditing: () => validateField("birthDate"),
           locale: language,
+          error: formErrors.birthDateError,
+          errorMessage: text.error["p.4"],
         },
         {
           linkId: "p.5",
           text: text["p.5"],
           value: registeredPatient.gender,
           type: "choice",
-          onSelect: (value) => dispatch(updateGender(value)),
+          onSelect: (value) => {
+            dispatch(updateGender(value));
+            setFormErrors({ ...formErrors, genderError: false });
+          },
           answerOption: [
             { valueCoding: { code: "male", display: text.male } },
             { valueCoding: { code: "female", display: text.female } },
             { valueCoding: { code: "other", display: text.other } },
           ],
+          error: formErrors.genderError,
+          errorMessage: text.error["p.5"],
         },
       ],
     },
   ];
 
-  return { registrationScreenData, text };
+  return { registrationScreenData, text, validateAllFields };
 };
 
 export default useRegistrationScreenData;
