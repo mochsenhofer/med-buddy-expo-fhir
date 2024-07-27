90877eaf8067487d872f3fd01981265c6bb61a64
mochsenhofer <123185718+mochsenhofer@users.noreply.github.com>
2024-07-27 08:36:02 +0200

    Update RegistrationScreen.js

--- src/screens/RegistrationScreen.js
+++ src/screens/RegistrationScreen.js
@@ -3,16 +3,26 @@ import BasicLayout from "../components/common/BasicLayout";
 import useRegistrationScreenData from "../hooks/useRegistrationScreenData";
 import { previewScreenRoute } from "../navigation/Navigation";
 import renderQuestionItem from "../utils/renderQuestionItem";
+import { useNavigation } from "@react-navigation/native";
 
 export default function RegistrationScreen() {
-  const { registrationScreenData, text } = useRegistrationScreenData();
+  const navigation = useNavigation();
+  const { registrationScreenData, text, validateAllFields } =
+    useRegistrationScreenData();
+
+  function handleNextButtonPressed() {
+    const isValid = validateAllFields();
+    if (isValid) {
+      navigation.navigate(previewScreenRoute);
+    }
+  }
 
   return (
     <BasicLayout
       title={text.title}
       sections={registrationScreenData}
       renderItem={renderQuestionItem}
-      navigateTo={previewScreenRoute}
+      onNextButtonPress={handleNextButtonPressed}
       behavior={"padding"}
     />
   );
