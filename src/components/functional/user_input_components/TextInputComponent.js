89f2f55b59d1fb6cdd47ae8d1f87636de09bcf87
mochsenhofer <123185718+mochsenhofer@users.noreply.github.com>
2024-07-27 08:35:21 +0200

    Update TextInputComponent.js

--- src/components/functional/user_input_components/TextInputComponent.js
+++ src/components/functional/user_input_components/TextInputComponent.js
@@ -1,21 +1,27 @@
 import React from "react";
 import { StyleSheet } from "react-native";
-import { TextInput } from "react-native-paper";
+import { TextInput, Text } from "react-native-paper";
+import ErrorMessage from "./ErrorMessage";
 
 export default function TextInputComponent({ item }) {
   return (
-    <TextInput
-      mode="outlined"
-      label={item.text}
-      onChangeText={item.onChangeText}
-      inputMode={item.type === "integer" ? "numeric" : "text"}
-      maxLength={item.maxLength}
-      value={item.value}
-      onSubmitEditing={item.onSubmitEditing}
-      ref={item.ref}
-      autoFocus={item.autoFocus}
-      style={userInputStyles.textInput}
-    />
+    <>
+      <TextInput
+        mode="outlined"
+        label={item.text}
+        onChangeText={item.onChangeText}
+        inputMode={item.type === "integer" ? "numeric" : "text"}
+        maxLength={item.maxLength}
+        value={item.value}
+        onSubmitEditing={item.onSubmitEditing}
+        onEndEditing={item.onEndEditing}
+        ref={item.ref}
+        autoFocus={item.autoFocus}
+        style={userInputStyles.textInput}
+        error={item.error}
+      />
+      {item.error && <ErrorMessage message={item.errorMessage} />}
+    </>
   );
 }
 
