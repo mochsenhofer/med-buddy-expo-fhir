92f96f341654e09bf521caf41401b1cb33b69a34
mochsenhofer <123185718+mochsenhofer@users.noreply.github.com>
2024-07-27 08:35:30 +0200

    Create useLanguage.js

---
+++ src/hooks/useLanguage.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { useSelector } from "react-redux";
+
+export default function useLanguage() {
+  const language = useSelector(
+    (state) => state.patient.communication[0].language.coding[0].code
+  );
+  return language;
+}
