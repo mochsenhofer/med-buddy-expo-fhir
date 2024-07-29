74064acecf9a26a8a5bc629d9508f7c6c4331d72
mochsenhofer <123185718+mochsenhofer@users.noreply.github.com>
2024-07-29 11:26:49 +0200

    Update renderQuestionItem.js

--- src/utils/renderQuestionItem.js
+++ src/utils/renderQuestionItem.js
@@ -3,16 +3,20 @@ import { Text } from "react-native-paper";
 import { p } from "../theme/constants";
 import renderUserInput from "./renderUserInput";
 import { StyleSheet } from "react-native";
+import SectionErrorMessage from "../components/functional/user_input_components/SectionErrorMessage";
 
 export default function renderQuestionItem({ item }) {
+  if (item.type === "error") {
+    return <SectionErrorMessage message={item.text} />;
+  }
+
   return (
     <>
-      {item.type === "choice" || item.type === "display" ? (
+      {(item.type === "choice" || item.type === "display") && (
         <Text style={styles.text} variant={p}>
           {item.text}
         </Text>
-      ) : null}
-
+      )}
       {renderUserInput(item)}
     </>
   );
