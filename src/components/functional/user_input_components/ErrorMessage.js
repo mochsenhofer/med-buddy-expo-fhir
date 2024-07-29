863b01fd35c58aad2e3075ef29212fe8944daee0
mochsenhofer <123185718+mochsenhofer@users.noreply.github.com>
2024-07-29 11:27:09 +0200

    Update ErrorMessage.js

--- src/components/functional/user_input_components/ErrorMessage.js
+++ src/components/functional/user_input_components/ErrorMessage.js
@@ -2,7 +2,7 @@ import { Text } from "react-native-paper";
 
 export default function ErrorMessage({ message }) {
   return (
-    <Text style={{ color: "red" }} variant="bodySmall">
+    <Text style={{ color: "red", margin: 2 }} variant="bodyMedium">
       {message}
     </Text>
   );
