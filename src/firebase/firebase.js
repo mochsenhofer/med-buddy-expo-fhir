43dc2654dc2b01d7e4fff78802e8cfdb64cb0537
mochsenhofer <123185718+mochsenhofer@users.noreply.github.com>
2024-07-31 14:35:00 +0200

    Create firebase.js

---
+++ src/firebase/firebase.js
@@ -0,0 +1,33 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
+import { getAuth } from "firebase/auth";
+import {
+  API_KEY,
+  AUTH_DOMAIN,
+  DATABASE_URL,
+  PROJECT_ID,
+  STORAGE_BUCKET,
+  MESSAGING_SENDER_ID,
+  APP_ID,
+  MEASUREMENT_ID,
+} from "@env";
+
+// Firebase configuration
+const firebaseConfig = {
+  apiKey: API_KEY,
+  authDomain: AUTH_DOMAIN,
+  databaseURL: DATABASE_URL,
+  projectId: PROJECT_ID,
+  storageBucket: STORAGE_BUCKET,
+  messagingSenderId: MESSAGING_SENDER_ID,
+  appId: APP_ID,
+  measurementId: MEASUREMENT_ID,
+};
+
+// Initialize Firebase
+export const FIREBASE_APP = initializeApp(firebaseConfig);
+// Get a reference to the database service
+export const FIREBASE_DB = getDatabase(FIREBASE_APP);
+// Get a reference to the auth service
+export const FIREBASE_AUTH = getAuth(FIREBASE_APP);
