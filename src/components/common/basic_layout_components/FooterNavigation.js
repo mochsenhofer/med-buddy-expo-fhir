c647abbc8a9a5fd291a9257b0d797f708ec78dfa
mochsenhofer <123185718+mochsenhofer@users.noreply.github.com>
2024-07-31 13:19:51 +0200

    Update FooterNavigation.js

--- src/components/common/basic_layout_components/FooterNavigation.js
+++ src/components/common/basic_layout_components/FooterNavigation.js
@@ -3,7 +3,9 @@ import { StyleSheet, View } from "react-native";
 import { Button, Text } from "react-native-paper";
 import { marginScreenBorderComponents } from "../../../theme/constants";
 import { useNavigation } from "@react-navigation/native";
+import useLanguage from "../../../hooks/useLanguage";
 import theme from "../../../theme/theme";
+import { texts } from "../../../languages/texts";
 
 export default function FooterNavigation({
   onNextButtonPress,
@@ -12,6 +14,8 @@ export default function FooterNavigation({
   page,
 }) {
   const navigation = useNavigation();
+  const language = useLanguage();
+  const text = texts[language].navigationButtons;
 
   function handleBackButtonPressed() {
     if (onBackButtonPress) {
@@ -38,7 +42,7 @@ export default function FooterNavigation({
         labelStyle={styles.navigationButtonText}
         onPress={handleBackButtonPressed}
       >
-        BACK
+        {text.back}
       </Button>
       <Text>{page}</Text>
       <Button
@@ -49,7 +53,7 @@ export default function FooterNavigation({
         labelStyle={styles.navigationButtonText}
         onPress={handleNextButtonPressed}
       >
-        NEXT
+        {text.next}
       </Button>
     </View>
   );
@@ -72,7 +76,6 @@ const styles = StyleSheet.create({
   },
   navigationButtonText: {
     fontSize: 20,
-    textTransform: "uppercase",
   },
   backButton: {
     position: "absolute",
