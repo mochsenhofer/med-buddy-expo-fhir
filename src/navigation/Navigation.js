d216a08de8fc355ae2039df86c7967668230c4e8
mochsenhofer <123185718+mochsenhofer@users.noreply.github.com>
2024-08-09 13:34:20 +0200

    Update Navigation.js

--- src/navigation/Navigation.js
+++ src/navigation/Navigation.js
@@ -10,7 +10,6 @@ import PreviewScreen from "../screens/PreviewScreen";
 import VideoScreen from "../screens/VideoScreen";
 import QuestionnaireScreen from "../screens/QuestionnaireScreen";
 import OverviewScreen from "../screens/OverviewScreen";
-import ConsentScreen from "../screens/ConsentScreen";
 import FaqScreen from "../screens/FaqScreen";
 
 export const startScreenRoute = "StartScreen";
@@ -20,7 +19,6 @@ export const previewScreenRoute = "PreviewScreen";
 export const videoScreenRoute = "VideoScreen";
 export const questionnaireScreenRoute = "QuestionnaireScreen";
 export const overviewScreenRoute = "OverviewScreen";
-export const consentScreenRoute = "ConsentScreen";
 export const faqScreenRoute = "FaqScreen";
 
 const screens = [
@@ -52,10 +50,7 @@ const screens = [
     route: overviewScreenRoute,
     component: OverviewScreen,
   },
-  {
-    route: consentScreenRoute,
-    component: ConsentScreen,
-  },
+
   {
     route: faqScreenRoute,
     component: FaqScreen,
