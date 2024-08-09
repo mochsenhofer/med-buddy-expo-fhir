036d599b74bfb35946e458e092f6f920b68bd651
mochsenhofer <123185718+mochsenhofer@users.noreply.github.com>
2024-08-09 12:54:25 +0200

    Update OverviewScreen.js

--- src/screens/OverviewScreen.js
+++ src/screens/OverviewScreen.js
@@ -3,16 +3,44 @@ import { Text } from "react-native-paper";
 import BasicLayout from "../components/common/BasicLayout";
 import { useSelector } from "react-redux";
 import { DevSettings } from "react-native";
+import { FIREBASE_DB } from "../firebase/firebase";
+import { push, ref, set } from "firebase/database";
+import { Alert } from "react-native";
+
+import useLanguage from "../hooks/useLanguage";
 
 export default function OverviewScreen() {
   const updatedQuestionnaireResponse = useSelector(
     (state) => state.questionnaireResponse
   );
+  const language = useLanguage();
   const DATA = [
     {
       data: [JSON.stringify(updatedQuestionnaireResponse, null, 2)],
     },
   ];
+
+  async function uploadCompletedQuestionnaire() {
+    try {
+      const db = FIREBASE_DB;
+      const questionnaireResponseRef = ref(db, "questionnaireResponses");
+      const newQuestionnaireResponseRef = push(questionnaireResponseRef);
+
+      await set(newQuestionnaireResponseRef, {
+        ...updatedQuestionnaireResponse,
+        id: newQuestionnaireResponseRef.key,
+        questionnaire: language,
+        hello: "world",
+      });
+
+      console.log("Data uploaded successfully");
+    } catch (error) {
+      console.error("Error adding document: ", error.message);
+      Alert.alert("Upload Error", "Failed to upload data. Please try again.");
+    } finally {
+      DevSettings.reload();
+    }
+  }
 
   const renderItem = ({ item }) => <Text>{item}</Text>;
 
@@ -21,7 +49,7 @@ export default function OverviewScreen() {
       title="Danke für Ihre Teilnahme!"
       sections={DATA}
       renderItem={renderItem}
-      onNextButtonPress={() => DevSettings.reload()}
+      onNextButtonPress={() => uploadCompletedQuestionnaire()}
     />
   );
 }
